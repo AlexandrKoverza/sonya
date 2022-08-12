b5a72ab73c99404773ac619783eaf5cb4b2bcfac
AlexandrKoverza <alexandrkoverza@gmail.com>
2022-08-13 00:25:21 +0300

    fix header

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,34 @@
 import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
+// Burger
+let burger = document.querySelector('.burger')
+let nav = document.querySelector('.header-bottom')
+
+burger.addEventListener('click', (e) => {
+    e.preventDefault()
+    burger.classList.toggle('burger-active')
+    nav.classList.toggle('active')
+})
+
+// Spoiler
+document.querySelectorAll('.spoiler-title').forEach((element) => {
+    element.addEventListener('click', () => {
+        let content = element.nextElementSibling
+        if(content.style.maxHeight) {
+            document.querySelectorAll('.spoiler-content').forEach((spoilerContent) => {
+                spoilerContent.style.maxHeight = null
+            })
+        } else {
+            document.querySelectorAll('.spoiler-content').forEach((spoilerContent) => {
+                spoilerContent.style.maxHeight = null
+                content.style.maxHeight = content.scrollHeight + 'px'
+            })
+        }
+    })
+});
+
+
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
