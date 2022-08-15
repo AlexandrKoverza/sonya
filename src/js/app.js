7b15cc24dbbfc5db048c92b1d0d2741926817a73
AlexandrKoverza <alexandrkoverza@gmail.com>
2022-08-15 22:17:24 +0300

    add new blocks

--- src/js/app.js
+++ src/js/app.js
@@ -28,6 +28,38 @@ document.querySelectorAll('.spoiler-title').forEach((element) => {
     })
 });
 
+// Tabs
+
+const tabs = document.querySelectorAll('.tabs')
+
+tabs.forEach(tab => {
+    const tabButtons = tab.querySelectorAll('.tabs-button')
+    const tabContents = tab.querySelectorAll('.tab-content')
+
+    tabButtons.forEach(tabButton => {
+        tabButton.addEventListener('click', () => {
+            let tabData = tabButton.getAttribute('data-tab')
+            let tabContent = tab.querySelector(tabData)
+
+            if (!tabButton.classList.contains('active')) {
+                tabButtons.forEach(item => {
+                    item.classList.remove('active')
+                })
+                tabContents.forEach(item => {
+                    item.classList.remove('active')
+                })
+
+                tabButton.classList.add('active')
+                tabContent.classList.add('active')
+
+            }
+
+
+        })
+    })
+
+    tab.querySelector('.tabs-button:nth-child(1)').click()
+})
 
 /*
 import Swiper, { Navigation, Pagination } from 'swiper';
